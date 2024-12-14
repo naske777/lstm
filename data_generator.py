5e1dfdfe3bfb939a0e894eb3fee7f64606e016ad
naske777 <naske777ig@gmail.com>
2024-12-13 22:55:54 -0500

    feat: Use btc data

--- data_generator.py
+++ data_generator.py
@@ -1,6 +1,7 @@
-
 import torch
 import numpy as np
+import json
+import math
 
 def normalize_data(data):
     min_val = min(data)
@@ -12,43 +13,25 @@ def denormalize_data(normalized_data, min_val, max_val):
     return [x * (max_val - min_val) + min_val for x in normalized_data]
 
 def generate_sequence_data(device):
-    sequence = [
-        # Pattern 1: Counting up by 1
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-        
-        # Pattern 2: Counting by 2
-        2, 4, 6, 8, 10, 12, 14, 16, 18, 20,
-        
-        # Pattern 3: Counting by 3
-        3, 6, 9, 12, 15, 18, 21, 24, 27, 30,
-        
-        # Pattern 4: Counting by 5
-        5, 10, 15, 20, 25, 30, 35, 40, 45, 50,
-        
-        # Pattern 5: Alternating +10/-5
-        10, 5, 15, 10, 20, 15, 25, 20, 30, 25,
-        
-        # Pattern 6: Symmetric around center
-        2, 4, 6, 8, 10, 10, 8, 6, 4, 2,
-        
-        # Pattern 7: Powers of 2
-        1, 2, 4, 8, 16, 32, 64, 128, 256, 512,
-        
-        # Pattern 8: Fibonacci-like
-        1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
-        
-        # Pattern 9: Squared numbers with sign alternating
-        1, -4, 9, -16, 25, -36, 49, -64, 81, -100,
-        
-        # Pattern 10: Palindromic pattern
-        5, 10, 15, 20, 25, 25, 20, 15, 10, 5
-    ]
+       # Leer el archivo JSON
+    with open('btc_price_1h.json', 'r') as file:
+        data = json.load(file)
     
-    normalized_sequence, min_val, max_val = normalize_data(sequence)
+    # Calcular el 10% de los datos
+    sample_size = math.ceil(len(data) * 1)
+    
+    # Extraer solo los últimos 10% de datos
+    sample_data = data[-sample_size:]
+    
+    # Extraer solo los precios de cierre (close)
+    close_prices = [float(entry.get('close', 0)) for entry in sample_data if 'close' in entry]
+    
+    normalized_sequence, min_val, max_val = normalize_data(close_prices)
     
     X, y = [], []
     for i in range(len(normalized_sequence)-5):
         X.append(normalized_sequence[i:i+5])
         y.append(normalized_sequence[i+5])
     
-    return torch.FloatTensor(X).unsqueeze(-1).to(device), torch.FloatTensor(y).to(device), sequence, min_val, max_val
+    return torch.FloatTensor(X).unsqueeze(-1).to(device), torch.FloatTensor(y).to(device), close_prices, min_val, max_val
+
